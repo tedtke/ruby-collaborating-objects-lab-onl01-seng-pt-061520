037c6a88ba83dbd2816af3799076ab3969b7769a
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-16 23:44:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,8 @@ class Song
   def self.new_by_filename(filename)
   # binding.pry
   # filename.split(/-/) [1] = Song.new(filename.split(/-/) [1])
-  Song.new(filename.split(/ - /) [1])
+  # Song.new(filename.split(/ - /) [1])
+  Song.new(filename.parse)
   # artist = filename.split(/ - /) [0]
   # @artist.name = self
   # filename.split[0] = @artist
