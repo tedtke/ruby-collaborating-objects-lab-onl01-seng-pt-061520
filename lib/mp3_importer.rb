d389ee6476d3d99e05240ad4c3dd4e33a6f1d0cd
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-17 00:14:11 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,12 @@
 #has many songs 
 class MP3Importer 
   attr_accessor :path
-  Dir.entries ""
+  # Dir.entries ("mp3s")
   def initialize(path)
     @path = path
+  end
+  
+  def files
     
   end
   
