831ba0720cffd19887975cf3dfe46647b8e270a8
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-17 00:05:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   
   def artist_name=(singer)
     self.artist = Artist.find_or_create_by_name(singer)
-    artist.add_song(self)
+    Artist.add_song(self)
     # binding.pry
   end
 
