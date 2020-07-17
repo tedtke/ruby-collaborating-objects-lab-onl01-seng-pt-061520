5c6041ff9e9e843fe7e0e4835abec38f5a7d0274
Teodoro Garcia III <tedtke514@gmail.com>
2020-07-17 00:25:31 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,14 +2,14 @@
 require 'pry'
 class MP3Importer 
   attr_accessor :path
-  # Dir.entries ("mp3s")
+  
   def initialize(path)
     @path = path
   end
   
   def files
-    Dir.entries ("spec/fixtures/mp3s/.mp3")
-    binding.pry
+    Dir.entries ("spec/fixtures/mp3s")
+    # binding.pry
   end
   
 end
